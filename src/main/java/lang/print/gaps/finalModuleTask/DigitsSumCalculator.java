9cf392f9a62f8e29bf67cf269e7731a066e02bd2
nodircha <madnodiras23@gmail.com>
2025-02-08 21:12:43 +0500

    final task

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,11 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        String num = String.valueOf(number);
+        int num1 = Integer.parseInt(num.substring(0, 1));
+        int num2 = Integer.parseInt(num.substring(1, 2));
+        int num3 = Integer.parseInt(num.substring(2, 3));
+        int num4 = Integer.parseInt(num.substring(3, 4));
+        System.out.println(num1+num2+num3+num4);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,10 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        String str = String.valueOf(number);
+        StringBuilder builder = new StringBuilder(str);
+        builder.reverse();
+        number = Integer.parseInt(builder.toString());
+        System.out.println(number);
     }
 }
